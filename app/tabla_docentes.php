95ca7bbff2a17eb7200b908cd9cc3cc1f72951ca
Jesus Denova <denovaj37@gmail.com>
2026-08-07 13:28:11 -0600

    correccion en tabla_docentes styles

--- app/tabla_docentes.php
+++ app/tabla_docentes.php
@@ -2,7 +2,6 @@
     background: rgb(219, 218, 207);
 }
     </style><?php
-
 session_start();
 
 require('conexion.php');
@@ -10,6 +9,8 @@ require('conexion.php');
 $rol = $_SESSION['rol'];
 
 $status = $_POST['status'] ?? '';
+
+$query = '';
 
 if($rol == 1){
 $query = "SELECT
@@ -24,79 +25,51 @@ id_docente, nombre, telefono, correo, status, num_empleado FROM docentes";
 
 }
 if ($status !== '') {
-    $query .= " AND status = '$status'";
+$query .= " AND status = '$status'";
 }
- 
 
 $resultado = $mysqli->query($query);
 
-echo "<div class='tabla-responsive'>
-        <table id='tablaalumnoo' class='display'>
-            <thead>
-                <tr>
-                    <th>Nombre</th>
-                    <th>Telefono</th>
-                    <th>Correo</th>
-                    <th>Num. Empleado</th>
-                     <th>Status</th>
-                    <th>Modificar</th>
-                </tr>
-            </thead>
-            <tbody>";
+echo "
+
+
+
+Nombre
+Telefono
+Correo
+Num. Empleado
+Status
+Modificar
+
+
+";
 
 while ($row = $resultado->fetch_assoc()) {
 
- echo "<tr>
-        <td>{$row['nombre']}</td>
-        <td>{$row['telefono']}</td>
-        <td>{$row['correo']}</td>
-         <td>{$row['num_empleado']}</td>
-        <td>";
+echo "
+{$row['nombre']}
+{$row['telefono']}
+{$row['correo']}
+{$row['num_empleado']}
+";
 ?>
 
-<form action="modificar_docente.php" method="POST">
-    <input type="hidden" name="id_docente" value="<?php echo $row['id_docente']; ?>">
-
+<input type="hidden" name="status" value="2">
 <?php
-if ($row['status'] == 1) {
-   
-    $textoBtn = "Activo";
-     $claseBtn = "btn-success"; 
-    
-    ?>
-    <input type="hidden" name="status" value="2">
-    <?php
+
 } elseif ($row['status'] == 2) {
-    
-   $textoBtn = "Inactivo";
-        $claseBtn = "btn-danger";
-    ?>
-    <input type="hidden" name="status" value="1">
-    <?php
-}
+
+$textoBtn = "Inactivo";
+$claseBtn = "btn-danger";
 ?>
 
-  <input type="submit" value="<?php echo $textoBtn; ?>" class="btn <?php echo $claseBtn; ?> btn-sm">
-</form>
 
 
+
+
+<input type="submit" value="Actualizar" class="btn btn-success btn-sm">
 
 <?php
-echo "</td>
 
-
-
-
- <td>";
-?>
-
-<form action="upd_docente.php" method="POST">
-    <input type="hidden" name="id_docente" value="<?php echo $row['id_docente']; ?>">
-
-    <input type="submit" value="Actualizar" class="btn btn-success btn-sm">
-</form></tr>
-<?php
-}
-
-echo "</tbody></table></div>";
+echo "";
 ?>
