bab1b679e112d58994c6c8ed3eb10bc8577d3047
Jesus Denova <denovaj37@gmail.com>
2026-08-07 03:27:44 -0600

    Cambiar conexión de InfinityFree a Railway 2.0

--- app/conexion.php
+++ app/conexion.php
@@ -1,20 +1,22 @@
 <?php
 
-//$host = getenv('DB_HOST') ?: 'mysql';
-$host = "sql202.infinityfree.com";
-$user = getenv('DB_USER') ?: 'admin';
-$pass = getenv('DB_PASS') ?: 'admin123';
-$db   = getenv('DB_NAME') ?: 'tecnol64_talleres2026V2';
+$host = getenv('DB_HOST') ?: 'localhost';
+$user = getenv('DB_USER') ?: 'root';
+$pass = getenv('DB_PASS') ?: '';
+$db   = getenv('DB_NAME') ?: 'railway';
 $port = getenv('DB_PORT') ?: '3306';
+
 
 $mysqli = new mysqli($host, $user, $pass, $db, $port);
 
-$mysqli->set_charset("utf8");
 
 if ($mysqli->connect_errno) {
     die("Error al conectar con la base de datos: (" .
         $mysqli->connect_errno . ") " .
         $mysqli->connect_error);
 }
+
+
+$mysqli->set_charset("utf8");
 
 ?>
